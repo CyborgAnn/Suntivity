50ca895ddc24de9ac4cfe09d017557c00b681452
Samuel Coleman <samuel.coleman@my.utsa.edu>
2026-07-28 17:10:11 -0500

    Enhance Item class with JavaDoc comments
    Added documentation comments for clarity and understanding.

--- Item.java
+++ Item.java
@@ -1,28 +1,65 @@
 package suntivity_model;
 
+/**
+ * Represents an item that can be purchased and used within the Suntivity
+ * application.
+ *
+ * <p>This abstract class provides common properties shared by all item types,
+ * including a name and the number of points required to purchase the item.
+ * Specific item types should extend this class and add their own functionality.</p>
+ */
 public abstract class Item {
 
+    /** The number of points required to purchase this item. */
     private int cost;
+
+    /** The name of the item. */
     private String name;
 
+    /**
+     * Creates a new item with the specified cost and name.
+     *
+     * @param cost the number of points required to purchase the item
+     * @param name the item name
+     */
     public Item(int cost, String name) {
         this.cost = cost;
         this.name = name;
     }
 
-    public void setCost(int cost){
+    /**
+     * Updates the purchase cost of this item.
+     *
+     * @param cost the new item cost
+     */
+    public void setCost(int cost) {
         this.cost = cost;
     }
 
-    public int getCost(){
+    /**
+     * Retrieves the number of points required to purchase this item.
+     *
+     * @return the item cost
+     */
+    public int getCost() {
         return cost;
     }
 
-    public void setName(String name){
+    /**
+     * Updates the name of this item.
+     *
+     * @param name the new item name
+     */
+    public void setName(String name) {
         this.name = name;
     }
 
-    public String getName(){
+    /**
+     * Retrieves the name of this item.
+     *
+     * @return the item name
+     */
+    public String getName() {
         return name;
     }
 }
