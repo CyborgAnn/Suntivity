a8c7e461a7f0bf4b85de2ab5b5fba01f1a2e66f7
Samuel Coleman <samuel.coleman@my.utsa.edu>
2026-07-28 17:12:15 -0500

    Enhance TaskStatus with documentation comments
    Added documentation comments for TaskStatus enum values.

--- TaskStatus.java
+++ TaskStatus.java
@@ -1,7 +1,26 @@
 package suntivity_model;
 
+/**
+ * Represents the current state of a task within the Suntivity application.
+ *
+ * <p>A task progresses through different states as it is completed by a child
+ * account and reviewed by a parent account.</p>
+ */
 public enum TaskStatus {
+
+    /**
+     * Indicates that the task has been created but has not yet been completed.
+     */
     INCOMPLETE,
+
+    /**
+     * Indicates that the child has completed the task and it is waiting for
+     * parent approval.
+     */
     PENDING,
+
+    /**
+     * Indicates that the task has been approved and completed successfully.
+     */
     COMPLETE
 }
