7d802e53fbe0f145873682251904d62330f39ba4
Samuel Coleman <samuel.coleman@my.utsa.edu>
2026-07-28 13:47:40 -0500

    Update Color class to use String for rgbHex
    Changed rgbHex type from int to String in Color class.

--- Color.java
+++ Color.java
@@ -2,18 +2,18 @@ package suntivity_model;
 
 public class Color extends Item {
 
-    private int rgbHex;
+    private String rgbHex;
 
-    public Color(int cost, String name, int rgbHex) {
+    public Color(int cost, String name, String rgbHex) {
         super(cost, name);
         this.rgbHex = rgbHex;
     }
 
-    public void setRgbHex(int rgbHex){
+    public void setRgbHex(String rgbHex){
         this.rgbHex = rgbHex;
     }
 
-    public int getRgbHex(){
+    public String getRgbHex(){
         return rgbHex;
     }
 }
