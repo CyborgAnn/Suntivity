86301ffc3e1690bb26ba3442116d13c0bcb2fb45
Samuel Coleman <samuel.coleman@my.utsa.edu>
2026-07-28 15:48:38 -0500

    Enhance model initialization with accounts and tasks
    Added initialization of parent and child accounts with tasks and points.

--- ModelInitializer.java
+++ ModelInitializer.java
@@ -5,7 +5,7 @@ public class ModelInitializer {
     //Initializes model without default accounts
     public static Model initializeModel(){
 
-        //Crease model object
+        //Create model object
         Model suntivityModel = new Model();
 
         //Initialize PlantStatus Objects
@@ -35,11 +35,11 @@ public class ModelInitializer {
         suntivityModel.addStoreItem(face6);
 
         //Initialize Box Objects
-        Box box1 = new Box(100, "Plant Pot 1", "/CharacterCustomization/PlantPot1");
-        Box box2 = new Box(100, "Plant Pot 2", "/CharacterCustomization/PlantPot2");
-        Box box3 = new Box(100, "Plant Pot 3", "/CharacterCustomization/PlantPot3");
-        Box box4 = new Box(100, "Plant Pot 4", "/CharacterCustomization/PlantPot4");
-        Box box5 = new Box(100, "Plant Pot 5", "/CharacterCustomization/PlantPot5");
+        Box box1 = new Box(100, "Plant Pot 1", "/CharacterCustomization/PlantPot1.png");
+        Box box2 = new Box(100, "Plant Pot 2", "/CharacterCustomization/PlantPot2.png");
+        Box box3 = new Box(100, "Plant Pot 3", "/CharacterCustomization/PlantPot3.png");
+        Box box4 = new Box(100, "Plant Pot 4", "/CharacterCustomization/PlantPot4.png");
+        Box box5 = new Box(100, "Plant Pot 5", "/CharacterCustomization/PlantPot5.png");
 
         //Add Box objects to model
         suntivityModel.addStoreItem(box1);
@@ -72,6 +72,58 @@ public class ModelInitializer {
         //Create model object and initialize model without default accounts
         Model suntivityModel = ModelInitializer.initializeModel();
 
+        //Initialize parent and child accounts
+        ParentAccount parent1 = new ParentAccount("Emperor Palpatine", "SomehowIReturned", 1, suntivityModel);
+        ChildAccount child1 = new ChildAccount("Darth Vader", "SlayerofYounglings9000", 1);
+        ChildAccount child2 = new ChildAccount("Stormtrooper 1", "ICan'tAim3000", 1);
+        ChildAccount child3 = new ChildAccount("Stormtrooper 2", "Empire4Life", 2);
+
+        //Initialize point amounts
+        child1.setPoints(1000);
+        child2.setPoints(5);
+        child3.setPoints(100);
+
+        //Initialize plant statuses
+        child1.editPlantStatus(suntivityModel.getPlantStatus("Best Health"));
+        child2.editPlantStatus(suntivityModel.getPlantStatus("Worst Health"));
+        child3.editPlantStatus(suntivityModel.getPlantStatus("Middle Health"));
+
+        //Purchase items for children
+        child1.purchaseItem(suntivityModel.getStoreItem("Red"));
+        child1.purchaseItem(suntivityModel.getStoreItem("Face 5"));
+        child1.purchaseItem(suntivityModel.getStoreItem("Plant Pot 5"));
+        child1.purchaseItem(suntivityModel.getStoreItem("Plant Pot 1"));
+        child3.purchaseItem(suntivityModel.getStoreItem("Face 1"));
+
+        //Equip items for children
+        child1.equipItem(child1.getItem("Red"));
+        child1.equipItem(child1.getItem("Face 5"));
+        child1.equipItem(child1.getItem("Plant Pot 5"));
+        child3.equipItem(child1.getItem("Face 1"));
+
+        //Add Accounts to model
+        suntivityModel.addAccount(parent1);
+        suntivityModel.addAccount(child1);
+        suntivityModel.addAccount(child2);
+        suntivityModel.addAccount(child3);
+
+        //Link children to parent
+        child1.linkToParent(parent1.getLinkingCode(), suntivityModel);
+        child2.linkToParent(parent1.getLinkingCode(), suntivityModel);
+        child3.linkToParent(parent1.getLinkingCode(), suntivityModel);
+
+        //Create tasks for children
+        Task task1_1 = new Task(child1, parent1, "Defeat Rebels", "I don't want these rebel scum getting anywhere near my Death Star. Destroy them at once!", 5, 25, 1200, 100);
+        Task task1_2 = new Task(child1, parent1, "Imperial Meeting", "I know you hate these meetings and that you would much rather be choking younglings, but they are essential to the operation of the empire.", 5, 26, 1600, 50);
+        Task task2_1 = new Task(child2, parent1, "Leave Death Star", "Your aim is abysmal, as a result you are fired from the Death Star and must leave immediately.", 5, 25, 1000, 1);
+        Task task2_2 = new Task(child2, parent1, "Aim Practice", "Your aim is abysmal, now must redo basic training at the imperial camp.", 6, 12, 800, 10);
+        Task task3_1 = new Task(child3, parent1, "Clean Garbage Compactor", "As punishment for letting the rebels escape, you must now clean up the garbage compactor.", 5, 26, 1800, 1);
+
+        //Set complete tasks
+        child2.completeTask(task2_1);
+        child3.completeTask(task3_1);
+
+        //return the model
         return suntivityModel;
     }
 }
